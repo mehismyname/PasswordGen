c4d1e327cb4aa919dc776c9d585f778656dd1127
Kent Smith <kent.smith@outlook.com>
2021-04-26 18:56:54 -0400

    Started the gen

---
+++ generator.py
@@ -0,0 +1,2 @@
+import random
+from tools import lower, upper, nums, special
---
+++ tools.py
@@ -0,0 +1,9 @@
+# alphabet characters
+lower='abcdefghijklmnopqrstuvwxyz'
+upper= lower.upper()
+
+# numbers
+nums= '0123456789'
+
+#specials
+special='!@#$%&_'
